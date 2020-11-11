9cc5a3561c574775e879f76feabd0b12ea2dcb56
jPee2k <kipishept@gmail.com>
2020-11-11 12:21:14 +0200

    updated regex / translated error messages

--- app/Http/Requests/StoreEmployee.php
+++ app/Http/Requests/StoreEmployee.php
@@ -24,13 +24,53 @@ class StoreEmployee extends FormRequest
     public function rules()
     {
         return [
-            // todo first_name & last_name -> regex: alpha - '
-            'first_name' => 'required|min:3|max:255|alpha',
-            'last_name' => 'required|min:3|max:255|alpha',
-            'patronymic' => 'nullable|min:3|max:255|alpha',
+            'first_name' => [
+                'required',
+                'min:2',
+                'max:255',
+                // Иван, Саша-Барон, Ахмед-шах
+                'regex:/^(?!-)(?!.*--)([A-ZА-ЯЁ]{1}[a-zа-я\-]+)+(?<!-)$/mu'
+            ],
+            'last_name' => [
+                'required',
+                'min:2',
+                'max:255',
+                'regex:/^(?!-)(?!.*--)([A-ZА-ЯЁ]{1}[a-zа-я\-]+)+(?<!-)$/mu'
+            ],
+            // 'first_name' => 'required|min:2|max:255|alpha',
+            // 'last_name' => 'required|min:2|max:255|alpha',
+            'patronymic' => 'nullable|min:4|max:255|alpha',
             'gender' => 'nullable',
             'salary' => 'nullable|numeric',
-            'departments' => 'required',
+            'departments' => 'required'
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'Поле с именем обязательно к заполнению!',
+            'first_name.min' => 'Имя не может содержать менее 2-х символов!',
+            'first_name.max' => 'Превышен лимит символов!',
+            'first_name.regex' => 'Допустимый формат имён: "Григорий", "Мак-Кинли", "Кемаль-паша"',
+
+            'last_name.required' => 'Поле с фамилией обязательно к заполнению!',
+            'last_name.min' => 'Фамилия не может содержать менее 2-х символов!',
+            'last_name.max' => 'Превышен лимит символов!',
+            'last_name.regex' => 'Допустимый формат фамилий: "Тихий", "Салтыкова-Щедрина"',
+
+            'patronymic.alpha' => 'Отчество может содержать только буквы!',
+            'patronymic.min' => 'Отчество не может содержать менее 4-х букв!',
+            'patronymic.max' => 'Превышен лимит символов!',
+
+            'salary.numeric' => 'Допустимы полько цифры!',
+
+            'departments.required' => 'Сотрудник должен входить в состав хотя бы одного подразделения!'
         ];
     }
 }
